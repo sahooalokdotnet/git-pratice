20aaa04561788ec03ccdf6863cb98006a025d7f4
sahooalokdotnet <sahooalok.net@gmail.com>
2024-04-04 23:20:47 +0530

    button added

--- Program.cs
+++ Program.cs
@@ -3,5 +3,7 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Welcome to git tutorial..");
+        Calculator cal = new();
+        Console.WriteLine(cal.Add(20,40));
     }
 }
