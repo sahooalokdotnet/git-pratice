0cb5aff912332cdb6a95aa6cdfd37fd05fc05d25
sahooalokdotnet <sahooalok.net@gmail.com>
2024-04-05 06:29:26 +0530

    restriction added

--- Employee.cs
+++ Employee.cs
@@ -1,6 +1,11 @@
 public class Employee
 {
     public string FName{get;set;}
-    public int Age{get;set;}
+    public int Age{get;set {
+        if(value>18)
+        {    
+            Age = value;
+        }}
+    }
     public string Email{get;set;}
 }
